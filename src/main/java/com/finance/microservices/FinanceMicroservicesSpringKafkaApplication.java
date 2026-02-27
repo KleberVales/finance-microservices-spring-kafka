eca8fcf6d06fa06750b692087a55458b8400f00d
Kleber Vales <klebervales.dev@gmail.com>
2026-02-27 11:10:28 -0300

    basic configuration files

---
+++ src/main/java/com/finance/microservices/FinanceMicroservicesSpringKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.finance.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceMicroservicesSpringKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceMicroservicesSpringKafkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/finance/microservices/FinanceMicroservicesSpringKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finance.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceMicroservicesSpringKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
